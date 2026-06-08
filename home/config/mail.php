c8c0f3bc68405a8ee2000af92cdebc51df2647e1
Luis Godino Wallace <lgodwal2312@g.educaand.es>
2026-06-08 19:18:51 +0200

    update mensaje de email

--- home/config/mail.php
+++ home/config/mail.php
@@ -112,8 +112,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'outfitgotfg@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'OutfitGo'),
     ],
 
 ];
